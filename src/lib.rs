6c3687b9373d85c3ff2c89b0dc882046fcc704d6
Dumindu Rajarathna <dumindu.rajarathna@lazada.com>
2017-08-20 15:33:59 +0700

    adding cfg test module

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,13 @@ pub fn hello() -> String {
 	("Hello, world!").to_string()
 }
 
-#[test]
-fn test_hello() {
-	assert_eq!(hello(), "Hello, world!");
+#[cfg(test)]
+mod tests {
+
+    use super::hello;
+
+    #[test]
+    fn test_hello() {
+        assert_eq!(hello(), "Hello, world!");
+    }
 }
